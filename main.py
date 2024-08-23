46111067172a8b006d13c44a2f7f9b0f35f66058
dealite <120561172+Knazzev@users.noreply.github.com>
2024-08-23 21:36:03 +0500

    Update main.py
    New Mud Racing and Mow and Trim games!

--- main.py
+++ main.py
@@ -22,7 +22,9 @@ games = {
     4: {'title': 'Train Miner', 'token': '82647f43-3f87-402d-88dd-09a90025313f', 'promo_id': 'c4480ac7-e178-4973-8061-9ed5b2e17954'},
     5: {'title': 'Merge Away', 'token' : '8d1cc2ad-e097-4b86-90ef-7a27e19fb833', 'promo_id': 'dc128d28-c45b-411c-98ff-ac7726fbaea4'},
     6: {'title': 'Twerk Race 3D', 'token': '61308365-9d16-4040-8bb0-2f4a4c69074c',  'promo_id': '61308365-9d16-4040-8bb0-2f4a4c69074c'},
-    7: {'title': 'Polysphere', 'token': '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71', 'promo_id': '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71'}
+    7: {'title': 'Polysphere', 'token': '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71', 'promo_id': '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71'}, 
+    8: {'title': 'Mow and Trim', 'token': 'ef319a80-949a-492e-8ee0-424fb5fc20a6', 'promo_id': 'ef319a80-949a-492e-8ee0-424fb5fc20a6'},
+    9: {'title': 'Mud Racing', 'token': '8814a785-97fb-4177-9193-ca4180ff9da8', 'promo_id': '8814a785-97fb-4177-9193-ca4180ff9da8'}
 }
 
 INTERVAL = 20
